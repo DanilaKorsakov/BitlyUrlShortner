68e6d7be502467ac7c937d4783d1b00de592d35c
DanilaKorsakov <kvashnindan@gmail.com>
2022-09-29 18:27:01 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,55 @@
+import os
+from urllib.parse import urlparse
+import requests
+import argparse
+from dotenv import load_dotenv
+
+load_dotenv()
+
+def shorten_link(token, link):
+    headers = {"Authorization": f"{token}"}
+    url = 'https://api-ssl.bitly.com/v4/shorten'
+    payload = {"long_url": link}
+    response = requests.post(url, headers=headers, json=payload)
+    response.raise_for_status()
+    return response.json()["link"]
+
+
+def count_clicks(token, link):
+    parsed_link = urlparse(link)
+    short_link= f"{parsed_link.netloc}{parsed_link.path}"
+  
+    headers = {"Authorization": f"{token}"}
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}/clicks/summary'
+    response = requests.get(url, headers=headers)
+    response.raise_for_status()
+    return response.json()['total_clicks']
+
+
+def is_bitlink(link, token):
+    parsed_link = urlparse(link)
+    short_link= f"{parsed_link.netloc}{parsed_link.path}"
+  
+    headers = {"Authorization": f"{token}"}
+    url = f'https://api-ssl.bitly.com/v4/bitlinks/{short_link}'
+    response = requests.get(url, headers=headers)
+    return response.ok
+
+
+def main():
+    bitly_token = os.getenv('BITLY_TOKEN')
+    parser = argparse.ArgumentParser()
+    parser.add_argument('link', help='Ваша ссылка')
+    args = parser.parse_args()
+    user_link = args.link
+    try:
+        if is_bitlink(user_link, bitly_token):
+            print(f'Количество переходов по ссылке битли: {count_clicks(bitly_token, user_link)}')
+        else:
+            print('Битлинк', shorten_link(bitly_token, user_link))
+    except requests.HTTPError as error:
+        print(error)
+
+
+if __name__ == "__main__":
+    main()
